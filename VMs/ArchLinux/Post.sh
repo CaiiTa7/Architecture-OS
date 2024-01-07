f7c3caa1b646aa22ec0bec37a4962bfc4c14176a
CaiiTa7 <98960052+CaiiTa7@users.noreply.github.com>
2024-01-07 16:32:36 +0100

    Fix post.sh

--- VMs/ArchLinux/Post.sh
+++ VMs/ArchLinux/Post.sh
@@ -2,12 +2,11 @@
 
 # function to set up user student
 add_user() {
-    # Création de l'utilisateur student
+    # Creating the student user
     useradd -m -G wheel -s /bin/zsh student
     echo "student:Tigrou007" | chpasswd
-    # Installation de sudo
-    pacman -S sudo --noconfirm
-    # Décommenter la ligne permettant aux membres du groupe wheel d'utiliser sudo
+
+    # Uncomment the line allowing members of the wheel group to use sudo
     sed -i 's/^# %wheel ALL=(ALL:ALL) ALL$/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 }
