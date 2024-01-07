b31779f09091aad3d9a853a43536cc37a5c15d33
CaiiTa7 <98960052+CaiiTa7@users.noreply.github.com>
2024-01-07 16:30:14 +0100

    final

--- VMs/ArchLinux/Chroot.sh
+++ VMs/ArchLinux/Chroot.sh
@@ -15,7 +15,7 @@ locale_timezone_keyboard_hostname() {
 
 # Function to install packages
 install_packages() {
-    pacman -S zsh neofetch python python3 networkmanager --noconfirm
+    pacman -S zsh neofetch python python3 networkmanager wget --noconfirm
     systemctl enable NetworkManager
 }
 
